427280853e608867e8dc266c0a836185f2729f38
Niteesh Saravanan <nsarava3@travelers.com>
2025-07-02 14:15:35 -0400

    added planets

--- react/src/components/Planets.jsx
+++ react/src/components/Planets.jsx
@@ -1,18 +1,78 @@
-import { useParams } from 'react-router-dom';
-import planets from '../assets/planets.json' 
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link, useNavigate, useParams } from "react-router-dom";
 
 const Planet = (props) => {
     const { id } = useParams();
 
-    const planet = planets.find(film => film.id === parseInt(id));
 
-    if (!planet) {
-        return <p>Film not found</p>;
+    const navigate = useNavigate();
+    const [planet, setPlanet] = useState({});
+    const [characters, setCharacters] = useState([]);
+    const [films, setFilms] = useState([]);
+    const navigateCharacter = (character) => {
+        navigate(`/character/${character.id}`)
     }
+    const navigateFilm = (film) => {
+        navigate(`/planet/${film.id}`)
+    }
+    const fetchFilms = async () => {
+        fetch(`http://localhost:3000/api/planets/${id}/films`)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('Data could not be fetched!');
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setFilms(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching films:', error);
+            });
+    }
+    const fetchCharacters = async () => {
+        fetch(`http://localhost:3000/api/films/${id}/characters`)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('Data could not be fetched!');
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setCharacters(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching films:', error);
+            });
+    }
+
+
+    const fetchPlanet = async () => {
+        fetch(`http://localhost:3000/api/planets/${id}/`)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('Data could not be fetched!');
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setPlanet(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching planet:', error);
+            });
+    }
+
+    useEffect(() => {
+        fetchPlanet();
+        fetchCharacters();
+        fetchFilms();
+    }, []);
+
 
     return (
         <>
-            <h1 id="name"></h1>
+            <h1 id="name">{planet.name}</h1>
             <section id="generalInfo">
                 <p>Population: {planet.population}</p>
                 <p>Climate: {planet.climate}</p>
@@ -20,11 +80,27 @@ const Planet = (props) => {
             </section>
             <section id="characters">
                 <h2>Characters that are from this planet:</h2>
-                <ul></ul>
+                <ul>
+                    {
+                        characters.map((character) => (
+                            <li key={character.id}>
+                                <button onClick={() => navigateCharacter(character)} className="btn btn-primary">{character.name}</button>
+                            </li>
+                        ))
+                    }
+                </ul>
             </section>
             <section id="films">
                 <h2>Films this planet appears in:</h2>
-                <ul></ul>
+                <ul>
+                    {
+                        films.map((film) => (
+                            <li key={film.id}>
+                                <button onClick={() => navigateFilm(film)} className="btn btn-primary">{film.title}</button>
+                            </li>
+                        ))
+                    }
+                </ul>
             </section>
         </>
     )
